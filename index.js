debbc2c55172bb10026d29f665b7085d6eb41efd
Eric Iron <erik.stalhammar@hyph.com>
2024-01-14 11:14:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js at the project root
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
